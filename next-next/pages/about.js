02b0b602bd9c9d971edec1a7db51a58aee42dcae
aline dias necchi ribeiro <alinenecchi@gmail.com>
2021-05-22 12:15:17 -0300

    add pages

---
+++ next-next/pages/about.js
@@ -0,0 +1,11 @@
+import Menu from '../components/Menu'
+function Home() {
+    return (
+        <div>
+        <Menu/>
+        About <br/>
+        </div>
+    )
+   
+  }
+export default Home
---
+++ next-next/pages/contact.js
@@ -0,0 +1,11 @@
+import Menu from '../components/Menu'
+function Home() {
+    return (
+        <div>
+            <Menu/>
+            Contact <br/>
+        </div>
+    )
+   
+  }
+export default Home
